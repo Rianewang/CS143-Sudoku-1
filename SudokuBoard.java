c6a55e1f083091510179d1aea38683e2cde02eaf
Rianewang <75764338+Rianewang@users.noreply.github.com>
2025-04-18 18:04:33 -0700

    Add files via upload
    Printing board

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,11 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SudokuBoard { 
    private int [][] board;
    
-   public SudokuBoard (String fileName) {
+   public SudokuBoard (String fileName) throws FileNotFoundException {
+      board = new int [9][9];
+      File f = new File(fileName);
+      Scanner s = new Scanner (f);
       
+      for (int r= 0; r<9; r++ ) {
+         if (s.hasNextLine()) {
+            String line = s.nextLine();
+            for (int c=0; c< 9; c++){
+               char num = line.charAt(c);
+               
+               if(num=='.') {
+                  board[r][c]=0;
+               } else {
+                  board[r][c]= num-'0';
+               }
+            }
+         }
+         
+      }
    }
    
    public String toString () {
-      return "";
+      String result = "";
+      for (int r=0;r<9;r++) {
+         if (r%3 ==0) {
+            result += "| - - - + - - - + - - - |\n";
+         }
+         for (int c=0; c<9; c++) {
+            if(c%3==0) {
+               result+="| ";
+            }
+            if (board[r][c]==0) {
+               result += ". ";
+            } else {
+               result += board[r][c] + " ";
+            }
+         }
+         result += "| \n";
+         
+      }
+      result += "| - - - + - - - + - - - |\n";
+      return result;
    }
 }
+
+/*
+# PROGRAM OUTPUT
+ | - - - + - - - + - - - |
+ | 2 . . | 1 . 5 | . . 3 | 
+ | . 5 4 | . . . | 7 1 . | 
+ | . 1 . | 2 . 3 | . 8 . | 
+ | - - - + - - - + - - - |
+ | 6 . 2 | 8 . 7 | 3 . 4 | 
+ | . . . | . . . | . . . | 
+ | 1 . 5 | 3 . 9 | 8 . 6 | 
+ | - - - + - - - + - - - |
+ | . 2 . | 7 . 1 | . 6 . | 
+ | . 8 1 | . . . | 2 4 . | 
+*/
